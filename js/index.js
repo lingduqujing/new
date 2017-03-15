2f8d262f82606fbb4a7782b9002045be8b6b29f0
lingduqujing <bslshl@163.com>
2017-03-15 14:28:48 +0800

    个人简历
    零瀆曲靖 lingduqujing 个人简历

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(function(){
+    //swiper≥ı ºªØ
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        direction: 'vertical',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 30,
+        mousewheelControl: true
+    });
+});
